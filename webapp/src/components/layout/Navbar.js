78ddc49b18c09da4dccc07958866a3e22d0fe4e1
ericGOD24 <ericobandeira@hotmail.com>
2019-04-03 00:02:57 -0300

    iniciando projeto pelo web app + adicionando navbar

---
+++ webapp/src/components/layout/Navbar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-nd navbar-dark bg-dark fixed-top">
+          <a>
+            Pokedex
+          </a>
+        </nav>
+      </div>
+    )
+  }
+}
