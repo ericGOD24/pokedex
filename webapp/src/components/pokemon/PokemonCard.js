6a6f3d0c7db1aa66853a7f8a1fecc04753f35a43
ericGOD24 <ericobandeira@hotmail.com>
2019-05-29 22:29:54 -0300

    adicionando mais pokemons, indice, imagem e alterando a primeira letra para maiúscula

--- webapp/src/components/pokemon/PokemonCard.js
+++ webapp/src/components/pokemon/PokemonCard.js
@@ -1,4 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+import styled from 'styled-components';
+
+const Sprite = styled.img`
+    width: 5em;
+    height: 5em;
+    `;
 
 export default class PokemonCard extends Component {
 
@@ -8,17 +15,38 @@ export default class PokemonCard extends Component {
         pokemonIndex:""
     };
 
-    render() {
-        const name = this.props.name;
-        const url = this.props.url;
+    componentDidMount(){
+        // const name = this.props.name;
+        // const url = this.props.url;
+        const {name, url} = this.props;
+        const pokemonIndex = url.split("/")[url.split("/").length - 2];
+        const imageUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`
 
-        // const {name, url} = this.props;
+    this.setState({
+        name,
+        imageUrl,
+        pokemonIndex
+    })
+    }
+
+    render() {
+        
         return (
             <div className="col-md-5 col-sm-6 mb-5">
                 <div className="card">
-                    <div className="card-header">
-                        <h1>{name}</h1>
-                    </div>
+                    <h5 className="card-header">{this.state.pokemonIndex}</h5>
+                        <Sprite className="card-img-top rounded nx-auto mt-2" 
+                        src={this.state.imageUrl}/>
+
+                        <div className="card-body nx-auto"><h6 className="card-title">
+                        {this.state.name
+                        .toLowerCase()
+                        .split(' ')
+                        .map(
+                            letter => letter.charAt(0).toUpperCase() + letter.substring(1))
+                            .join(' ')}
+                            </h6>
+                        </div>
                 </div>
             </div>
         )
