0383c2858043ba7a4acadff21ea54af913057d9c
ericGOD24 <ericobandeira@hotmail.com>
2019-06-02 15:15:24 -0300

    adicionando cores aos tipos

--- webapp/src/components/pokemon/Pokemon.js
+++ webapp/src/components/pokemon/Pokemon.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+const TYPE_COLORS = {
+    bug: 'B1C12E',
+    dark: '4F3A2D',
+    dragon: '755EDF',
+    electric: 'FCBC17',
+    fairy: 'F4B1F4',
+    fighting: '823551D',
+    fire: 'E73B0C',
+    flying: 'A3B3F7',
+    ghost: '6060B2',
+    grass: '74C236',
+    ground: 'D3B357',
+    ice: 'A3E7FD',
+    normal: 'C8C4BC',
+    poison: '934594',
+    psychic: 'ED4882',
+    rock: 'B9A156',
+    steel: 'B5B5C3',
+    water: '3295F6'
+  };
+
 export default class Pokemon extends Component {
 
     state={
@@ -161,8 +182,12 @@ export default class Pokemon extends Component {
                                     <div className="float-right">
                                         {this.state.type.map(type => (
                                             <span key={type}
-                                            className="badge badge-primary badge-pill mr-1">
-                                                {type}
+                                            className="badge badge-primary badge-pill mr-1"
+                                            style={{background: `#${TYPE_COLORS[type]}`,color:'white'}}>
+                                                {type.toLowerCase()
+                                                .split("-")
+                                                .map(s=>s.charAt(0).toUpperCase() + s.substring(1))
+                                                .join(' ')}
                                             </span>
                                         ))}
                                     </div>
